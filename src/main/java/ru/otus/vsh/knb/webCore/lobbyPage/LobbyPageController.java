2511401e78ca4e581f388675cd4ada9a7d08a16b
Viktoria Shirunova <viktoria.shirunova@jetbrains.com>
2020-09-07 19:56:10 +0300

    [auth] webCore: added lobby page and its controller

---
+++ src/main/java/ru/otus/vsh/knb/webCore/lobbyPage/LobbyPageController.java
@@ -0,0 +1,31 @@
+package ru.otus.vsh.knb.webCore.lobbyPage;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.otus.vsh.knb.dbCore.model.Person;
+import ru.otus.vsh.knb.msCore.common.Id;
+import ru.otus.vsh.knb.webCore.Routes;
+
+@Controller
+@AllArgsConstructor
+@Slf4j
+public class LobbyPageController {
+
+    private static final String TEMPLATE_PLAYER_NAME = "playerName";
+    private static final String TEMPLATE_SESSION_ID = "sessionId";
+    private static final String LOBBY_HTML = "lobby.html";
+
+    @GetMapping(Routes.LOBBY)
+    public String getGamePage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute(TEMPLATE_PLAYER_NAME, ((Person) authentication.getPrincipal()).getName());
+        model.addAttribute(TEMPLATE_SESSION_ID, new Id().getInnerId());
+
+        return LOBBY_HTML;
+    }
+}
