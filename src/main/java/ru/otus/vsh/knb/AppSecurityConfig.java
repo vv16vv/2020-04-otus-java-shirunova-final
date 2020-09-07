6e7e0d2a4fc3d117261caf45c4f87894bfa8a394
Viktoria Shirunova <viktoria.shirunova@jetbrains.com>
2020-09-07 19:53:55 +0300

    [auth] webCore: configured security

---
+++ src/main/java/ru/otus/vsh/knb/AppSecurityConfig.java
@@ -0,0 +1,51 @@
+package ru.otus.vsh.knb;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.otus.vsh.knb.dbCore.dbService.DBServicePerson;
+import ru.otus.vsh.knb.webCore.Routes;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final DBServicePerson dbServicePerson;
+
+    @Autowired
+    protected void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(dbServicePerson)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/*.css", "/*.js").permitAll()
+                .antMatchers(Routes.NEW_PLAYER).permitAll()
+                .antMatchers(Routes.ROOT).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage(Routes.ROOT)
+                .loginProcessingUrl(Routes.API_LOGIN)
+                .defaultSuccessUrl(Routes.LOBBY, true)
+                .failureUrl(Routes.ERROR)
+        ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/ru/otus/vsh/knb/webCore/Routes.java
@@ -0,0 +1,20 @@
+package ru.otus.vsh.knb.webCore;
+
+public final class Routes {
+
+    public static final String ROOT = "/";
+    public static final String PLAYERS = "/players";
+    public static final String NEW_PLAYER = "/new-player";
+    public static final String LOBBY = "/lobby";
+    public static final String GAME = "/game";
+
+    public static final String API = "/api";
+    public static final String API_LOGIN = API + "/login";
+    public static final String API_GAME_WS = API + "/game-ws";
+    public static final String API_GAME_START = "/game-start";
+    public static final String API_ANSWER = "/answer";
+
+    public static final String API_TOPIC = "/topic";
+
+    public static final String ERROR = "/error";
+}
